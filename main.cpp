61068f620dd84d3c833c3e3a8a8968dc149bc877
jyaritta <rahmasyaritazaya@gmail.com>
2026-03-31 19:18:07 +0800

    fibonacci

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <chrono>
+#include <iomanip>
 
 using namespace std;
 using namespace std::chrono;
 
 // iteratif
 int iteratif(int n){
-    long long f1 = 1, f2 = 2, temp;
+    long long f1 = 0, f2 = 1, temp;
     for(int i = 2; i <= n; i++){
         temp = f1 + f2;
         f1 = f2;
@@ -17,12 +18,51 @@ int iteratif(int n){
     return f2;
 }
 
+// rekursif dengan deret
+long long temp[100] = {0};
+long long rekursif(int n) {
+    if (n <= 1) return n;
+
+    if (temp[n] != 0) return temp[n];
+    temp[n] = rekursif(n - 1) + rekursif(n - 2);
+    return temp[n];
+}
+
+// rekursif F(n) = F(n−1) + F(n−2)
+long long rekursif_standar(int n) {
+    if (n <= 1) return n;
+    return rekursif_standar(n - 1) + rekursif_standar(n - 2);
+}
+
 int main(){
     int angka[] = {10, 20, 30, 40, 50};
-    auto start = high_resolution_clock::now();
+    
+    cout << "Metode Iteratif\n";
     for(int n : angka){
+        auto start = high_resolution_clock::now();
         iteratif(n);
+        auto end = high_resolution_clock::now();
+        duration<double> durasi = end - start;
+        cout << "\nWaktu Eksekusi: " << fixed << setprecision(9) << durasi.count() << " detik" << endl;
     }
-    auto end = high_resolution_clock::now();
-    cout << "\nWaktu Eksekusi: " << selisih.count() << " detik" << endl;
+
+    cout << "\nMetode Rekursif dengan Deret\n";
+    for(int n : angka){
+        auto start = high_resolution_clock::now();
+        cout << rekursif(n);
+        auto end = high_resolution_clock::now();
+        duration<double> durasi = end - start;
+        cout << "\nWaktu Eksekusi: " << fixed << setprecision(9) << durasi.count() << " detik" << endl;
+    }
+
+    cout << "\nMetode Rekursif Standar\n";
+    for(int n : angka) {
+        auto start = high_resolution_clock::now();
+        long long hasil = rekursif_standar(n); 
+        cout << hasil;
+        auto end = high_resolution_clock::now();
+        duration<double> durasi = end - start;
+        cout << "\nWaktu Eksekusi: " << fixed << setprecision(9) << durasi.count() << " detik" << endl;
+    }
+    return 0;
 }
